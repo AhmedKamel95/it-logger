ff3228b647e75e5f94a5f4716645ffa185d95291
ahmedkamel095 <ahmedkamel095@gmail.com>
2019-12-20 18:16:44 +0200

    Finished Application

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React, { useEffect } from "react";
-import "materialize-css/dist/css/materialize.min.css";
-import M from "materialize-css/dist/js/materialize.min.js";
-import SearchBar from "./components/layout/SearchBar";
-import Logs from "./components/logs/Logs";
-import AddBtn from "./components/layout/AddBtn";
-import AddLogModal from "./components/logs/AddLogModal";
-import EditLogModal from "./components/logs/EditLogModal";
-import AddTechModal from "./components/techs/AddTechModal";
-import TechListModal from "./components/techs/TechListModal";
-import { Provider } from "react-redux";
-import store from "./store";
+import React, { Fragment, useEffect } from 'react';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
+import { Provider } from 'react-redux';
+import store from './store';
+
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import './App.css';
 
 const App = () => {
   useEffect(() => {
-    // Initialized Materialize JS
+    // Init Materialize JS
     M.AutoInit();
   });
-
   return (
     <Provider store={store}>
-      <>
+      <Fragment>
         <SearchBar />
-        <div className="container">
+        <div className='container'>
           <AddBtn />
           <AddLogModal />
           <EditLogModal />
@@ -29,7 +30,7 @@ const App = () => {
           <TechListModal />
           <Logs />
         </div>
-      </>
+      </Fragment>
     </Provider>
   );
 };
--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,13 +1,14 @@
-import React, { useEffect } from "react";
-import { connect } from "react-redux";
-import LogItem from "./LogItem";
-import Preloader from "../layout/Preloader";
-import { getLogs } from "../../actions/logActions";
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import LogItem from './LogItem';
+import Preloader from '../layout/Preloader';
+import PropTypes from 'prop-types';
+import { getLogs } from '../../actions/logActions';
 
 const Logs = ({ log: { logs, loading }, getLogs }) => {
   useEffect(() => {
     getLogs();
-    //eslint-disable-next-ling
+    // eslint-disable-next-line
   }, []);
 
   if (loading || logs === null) {
@@ -15,12 +16,12 @@ const Logs = ({ log: { logs, loading }, getLogs }) => {
   }
 
   return (
-    <ul className="collection with-header">
-      <li className="collection-header">
-        <h4 className="center">System Logs</h4>
+    <ul className='collection with-header'>
+      <li className='collection-header'>
+        <h4 className='center'>System Logs</h4>
       </li>
-      {!loading && logs && logs.length === 0 ? (
-        <p className="center">No logs to show...</p>
+      {!loading && logs.length === 0 ? (
+        <p className='center'>No logs to show...</p>
       ) : (
         logs.map(log => <LogItem log={log} key={log.id} />)
       )}
@@ -28,12 +29,16 @@ const Logs = ({ log: { logs, loading }, getLogs }) => {
   );
 };
 
+Logs.propTypes = {
+  log: PropTypes.object.isRequired,
+  getLogs: PropTypes.func.isRequired
+};
+
 const mapStateToProps = state => ({
   log: state.log
 });
 
-const mapDispatchToProps = dispatch => ({
-  getLogs: () => dispatch(getLogs())
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Logs);
+export default connect(
+  mapStateToProps,
+  { getLogs }
+)(Logs);
